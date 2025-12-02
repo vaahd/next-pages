2136d8e77159751d24dd9c2002a9f70587e7c151
vahid <vaahd890@gmail.com>
2025-12-02 11:34:15 +0530

    Fix bootstrap type error with ts-ignore

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ import type { AppProps } from 'next/app';
 export default function MyApp({ Component, pageProps }: AppProps) {
 
   useEffect(() => {
+    //  @ts-ignore 
     import("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
 
@@ -35,3 +36,4 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     </DarkModeProvider>
   );
 }
+
