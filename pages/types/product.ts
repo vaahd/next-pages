7f45cf1d5043c51d5385e06b170b194669a54329
vahid <vaahd890@gmail.com>
2025-12-02 09:25:07 +0530

    Fix product type: add default export

--- pages/types/product.ts
+++ pages/types/product.ts
@@ -1,9 +1,11 @@
-export interface Product {
-  id: number
-  title: string
-  price: number
-  description: string
-  category: string
-  image: string
-  rating?: { rate: number; count: number }
-}
+export type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating?: { rate: number; count: number };
+};
+
+export default Product;
