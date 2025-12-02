e9a51fcf1839d30a308756a8d5f570b7f9a2b46d
vahid <vaahd890@gmail.com>
2025-12-02 12:42:23 +0530

    Created

--- pages/components/ProductCard.tsx
+++ pages/components/ProductCard.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Link from 'next/link'
-import { Product } from '../types/product'
+import { Product } from '../../types/product'
 import { useState } from 'react'
 
 export default function ProductCard({ product }: { product: Product }) {
